886fcc527f39641f96af0323a8e6f5a761c8b019
Emediong Bassey Edem <edememediong1@gmail.com>
2026-01-23 23:39:22 +0100

    feat: two sum bruteforce approach without hashmap

--- 04-leetcode/arrays-hashing/two-sum.ts
+++ 04-leetcode/arrays-hashing/two-sum.ts
@@ -9,9 +9,16 @@
  * Space Complexity: O(n)
  */
 
-function twoSum(nums: number[], target: number): number[] {
+function twoSum(nums: number[], target: number) {
   // TODO: Implement solution
-  return [];
+  for (let i = 0; i < nums.length; i++ ){
+    for (let j = i + 1; j < nums.length; j++){
+      let sum = nums[i] + nums[j]
+      if (sum == target){
+          return [i, j];
+      }
+    }
+  }
 }
 
 // Test cases
